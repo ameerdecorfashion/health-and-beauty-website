ad01d05f327bb2275ea699ce1c6d309749a7e181
ameerdecorfashion <ameer.decorfashion@gmail.com>
2026-05-16 17:54:47 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { CartProvider } from "./context/CartContext";
 
 import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
-import Login from "./pages/Login";
-import Admin from "./pages/Admin";
 
 export default function App() {
   return (
@@ -13,8 +11,6 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/checkout" element={<Checkout />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
     </CartProvider>
