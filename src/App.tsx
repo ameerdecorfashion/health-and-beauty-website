6d68328a5ed0c77de47198f7df59d9a51fcd61ed
ameerdecorfashion <ameer.decorfashion@gmail.com>
2026-05-16 17:39:40 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
+
 import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+
 export default function App() {
   return (
     <CartProvider>
@@ -9,9 +13,10 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
-      {/* We can add a global Toaster here if we implement shadcn toast later */}
     </CartProvider>
   );
 }
