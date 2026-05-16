221eac37848f6874a65a70a229168f442b2d9126
ameerdecorfashion <ameer.decorfashion@gmail.com>
2026-05-16 17:59:39 +0500

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,7 @@ export default function Login() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Login Page</h1>
-      <p>Firebase login will be connected here</p>
+      <p>Firebase login will be added here</p>
     </div>
   );
 }
