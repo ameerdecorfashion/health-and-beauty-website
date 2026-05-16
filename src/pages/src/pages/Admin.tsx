8fbb4c6ee432a7588de583e0241f8f65df8d9077
ameerdecorfashion <ameer.decorfashion@gmail.com>
2026-05-16 17:48:34 +0500

    Create Admin.tsx

---
+++ src/pages/src/pages/Admin.tsx
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Admin Dashboard</h1>
+      <p>Manage products, orders, and website content here</p>
+    </div>
+  );
+}
